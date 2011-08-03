1691ece32a2c2029d69b29f5b8f1044a07fccda8
Clement Lim <clement@clementlim.tel>
2011-08-03 11:52:57 -0700

    adding website content

---
+++ website/aws/index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title>Clement Lim</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    			
+    <!--[if IE]>
+    	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    
+    <!--[if IE 7]>
+    	<link rel="stylesheet" href="ie7.css" type="text/css" media="screen" />
+    <![endif]-->
+    
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+	<link rel="icon" type="image/x-ico" href="http://www.clementlim.com/favicon.ico" />
+	<link rel="shortcut icon" type="image/x-icon" href="http://www.clementlim.com/favicon.ico" />    
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.0/jquery.min.js" type="text/javascript"></script>
+    <script src="js/jquery.anchor.js" type="text/javascript"></script>
+    <script src="js/jquery.fancybox-1.2.6.pack.js" type="text/javascript"></script>
+
+	<script type="text/javascript">
+
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-24603388-1']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+	</script>
+	
+</head>
+
+<body>
+	<header> <!-- HTML5 header tag -->
+    
+    	<div id="headercontainer">
+    
+    		<h1><a class="introlink anchorLink" href="#intro"><i>Clement Lim</i></a></h1>
+    		
+    		<nav> <!-- HTML5 navigation tag -->
+    			<ul>
+    				<li><a class="introlink anchorLink" href="#intro"><b>Intro</b></a></li>
+    				<li><a class="professionallink anchorLink" href="#professional"><b>Professional</b></a></li>
+    				<li><a class="personallink anchorLink" href="#personal"><b>Personal</b></a></li>
+    				<li><a class="contactlink anchorLink" href="#contact"><b>Contact</b></a></li>
+    			</ul>				
+    		</nav>
+    	
+    	</div>
+    
+    </header>
+
+    <section id="contentcontainer"> <!-- HTML5 section tag for the content 'section' -->
+    
+    	<section id="intro">
+    	
+			<div align="left"><img src="images/clement_lim.png" align="left" style='vertical align: center'><h2 class="intro">A <strong>SHORT</strong> and <strong>SWEET</strong> blurb on who I am and what I do. <span class="sub">Both personally and professionally.</span></h2></div>
+    		    				
+    	</section>
+
+    	<section id="professional"> <!-- HTML5 section tag for the portfolio 'section' -->
+    	
+    		<h2 class="work">Professional</h2>		
+    			
+   				<a href="http://www.deloitte.com"><img src="images/deloitte.jpg" alt="" style='float: left'/></a><div style="margin-left: 200px;"><p><strong>Company: Deloitte</strong><br /><b>Title:</b> Consultant <br /><b> Duration:</b> 2011 - Present <br /><i>Reference: None</i></p></div>
+				<br /><br />
+   				<a href="http://www.fincad.com"><img src="images/fincad.jpg" alt="" style='float: left'/></a><div style="margin-left: 200px;"><p><strong>Company: FinancialCAD</strong><br /><b>Title:</b> Software Engineer <br /><b> Duration:</b> 2007 - 2010 <br /><i>Reference: <a href="https://s3.amazonaws.com/www.clementlim.com/references/FINCAD_Reference.pdf">FinancialCAD Letter</a></i></p></div>
+				<br /><br />
+   				<a href="http://www.metalogix.com"><img src="images/metalogix.png" alt="" style='float: left'/></a><div style="margin-left: 200px;"><p><strong>Company: Metalogix</strong><br /><b>Title:</b> Software Developer<br /><b>Duration:</b> Jan - Apr 2007<br /><i>Reference: <a href="https://s3.amazonaws.com/www.clementlim.com/references/Metalogix_Reference.pdf">Metalogix Letter</a></i></p></div>
+				<br /><br />
+   				<a href="http://www.ubc.com"><img src="images/ubc.jpg" alt="" style='float: left'/></a><div style="margin-left: 200px;"><p><strong>Company: University of British Columbia</strong><br /><b>Title:</b> Web Developer<br /><b>Duration:</b> Sept - Dec 2006<br /><i>Reference: <a href="https://s3.amazonaws.com/www.clementlim.com/references/UBC_Reference.pdf">UBC Letter</a></i></p></div>
+				<br /><br /><br />
+
+				<p><font size="6" color="#ff5400"><strong>Other Downloads:</strong></font></p>
+				<li><p><i><a href="https://s3.amazonaws.com/www.clementlim.com/docs/Clement_Lim_Resume.pdf">Current Résumé</a></i></p></li>
+    				
+    	</section>
+    			
+    	<section id="personal"> <!-- HTML5 section tag for the about 'section' -->
+    	
+    		<h2 class="about" >Personal</h2>
+    		
+    		<p>I believe that life is too short to be spent waiting for the stars to align before taking action. This is especially true for the scarier, bigger, more challenging things in life. I would rather give things a try, and be wrong, than be paralyzed by the fear of failure at the starting gates. As such, I have pushed myself into as many different life experiences as possible to discover my inner boundaries. My journey is far from over and I look forward to the next challenge over the next hill. Carpe diem!</p>
+			<br />
+			<font size="6" color="#ff5400"><strong>Interesting things done to date:</strong></font>
+			<br /><br />
+			<li>Volunteered as a Big Brother for 3 years</li>
+			<li>Hiked the West Coast Trail twice</li>
+			<li>Bungee jumped and skydived</li>
+			<li>Open Water Scuba Diving certification from PADI</li>
+			<li>Triathlon coaching certification from NCCP</li>
+			<li>Have a motorcycle license</li>
+			<li>Competed in Ironman Canada, Singapore Half Iron, Vancouver Marathon amongst many other races</li>
+			<li>Cycled across Canada; from Vancouver Island to Newfoundland</li>
+			<li>Countries visited: Singapore, Malaysia, Indonesia, Thailand, China, Australia, New Zealand, Japan, Canada, United States, England, Netherlands, Germany, Czech Republic, Slovakia, Austria, Hungary, Italy, Greece, Egypt, Vatican, Mexico, Dominican Republic, India, Nepal, Bhutan, Tibet, France, Switzerland, Spain</li>
+    	</section>
+    			
+    	<section id="contact"> <!-- HTML5 section tag for the contact 'section' -->
+    	
+    		<h2 class="contact">Contact Me</h2>
+			<a href="http://www.clementlim.tel" target="_blank"><img src="images/telnic.png" alt="telnic"></a>
+			<a href="http://ca.linkedin.com/in/clementl" target="_blank"><img src="images/linkedin.png" alt="linkedin"></a>
+			<a href="http://www.facebook.com/clement.lim" target="_blank"><img src="images/facebook.png" alt="facebook"></a>
+			<a href="http://www.twitter.com/limclement" target="_blank"><img src="images/twitter.png" alt="twitter"></a>
+			<a href="skype:clementlim.tel?call"><img src="images/skype.png" alt="skype"></a>
+			<a href="gtalk:chat?jid=clement@clementlim.tel"><img src="images/gchat.jpg" alt="gchat"></a>
+			<a href="msnim:chat?contact=clement@clementlim.com"><img src="images/msn.jpg" alt="msn"></a>
+			<br /><br />
+
+ 	   		<form id="contact" method="post" action="email.php"> 
+
+    			<p><label for="name">Name</label></p>
+    			<input type="text" id=name name=name placeholder="First and last name" required tabindex="1" /> 
+    			 
+    			<p><label for="email">Email</label></p> 
+    			<input type="text" id=email name=email placeholder="example@domain.com" required tabindex="2" /> 
+    			 
+    			<p><label for="comment">Your Message</label></p> 
+    			<textarea name="comment" id="comment" placeholder="Type message here" tabindex="4"></textarea> 
+    			 
+    			<input name="submit" type="submit" id="submit" tabindex="5" value="Send Message" /> 
+    			 
+    		</form> 
+			
+    	</section>
+    			
+    	<footer> <!-- HTML5 footer tag -->
+			<p><font size="4" color="#4b4b4b">Copyright © 2011 Clement Lim. All rights reserved.</font></p>
+    	</footer>	
+    
+    </section>
+
+	</body>
+
+</html>
